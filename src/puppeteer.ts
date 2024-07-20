f0242c5c01ac4c18cc156a10d11ae7871477a014
Isaac <Kiyoonewton41@gmail.com>
2024-07-20 08:44:35 +0100

    working serverless deployed version

--- src/puppeteer.ts
+++ src/puppeteer.ts
@@ -1,9 +1,18 @@
-const puppeteer = require("puppeteer");
-require("dotenv").config();
+import chromium from "@sparticuz/chromium-min";
+import puppeteer from "puppeteer-core";
 
-const apiEndpoint = "https://s5.sir.sportradar.com/bet9javirtuals/en/1/category/1111";
+const apiEndpoint =
+  "https://s5.sir.sportradar.com/bet9javirtuals/en/1/category/1111";
 module.exports = async (vflId: number) => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath(
+      "https://github.com/Sparticuz/chromium/releases/download/v126.0.0/chromium-v126.0.0-pack.tar",
+    ),
+    headless: chromium.headless,
+    ignoreHTTPSErrors: true,
+  });
   const page = await browser.newPage();
   await page.setUserAgent(
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
@@ -13,6 +22,8 @@ module.exports = async (vflId: number) => {
     "Accept-Language": "en-US,en;q=0.9",
   });
 
+  console.log("Chromium:", await browser.version());
+  console.log("Page Title:", await page.title());
   const bunPath = `#sr-container > div > div > div.container.container-main.contair-full-height-flex-auto > div > div > div > div > div > span > div > div > div > div > div > a:nth-child(${vflId})`;
   const achivePath =
     "#sr-container > div > div > div.menu-wrapper.menu-full-width-bg.menu-mobile-top.menu-mobile-sticky > div.container.no-padding > ul > li:nth-child(6) > a";
@@ -20,9 +31,18 @@ module.exports = async (vflId: number) => {
     "#sr-container > div > div > div.container.container-main.contair-full-height-flex-auto > div > div > div > div > div.panel.margin-bottom > div > div > div:nth-child(1) > table > tbody > tr:nth-child(2)";
   await page.goto(apiEndpoint);
 
-  await page.locator(bunPath).click();
-  await page.locator(achivePath).click();
-  await page.locator(clickFormCell).click();
+  const bunPathHandle = await page.$(bunPath);
+  if (bunPathHandle) {
+    await bunPathHandle.click();
+  }
+  const achivePathHandle = await page.$(achivePath);
+  if (achivePathHandle) {
+    await achivePathHandle.click();
+  }
+  const clickFormCellHandle = await page.$(clickFormCell);
+  if (clickFormCellHandle) {
+    await clickFormCellHandle.click();
+  }
   const currentUrl = page.url();
   const seasonKey = currentUrl.substring(currentUrl.lastIndexOf("/") + 1);
   await browser.close();
